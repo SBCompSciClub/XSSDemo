2e8b3e62308ffd8e7d3eca98cb9204b157eff364
RajatP314 <10013174@sbstudents.org>
2019-02-20 20:40:37 -0500

    added adin page

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.static('res'));
 app.all('/', (req, res)=>{
 	res.sendFile(__dirname + "/index.html");
 });
+app.all('/admin', (req, res)=>{
+	res.sendFile(__dirname + "/admin.html");
+});
 
 io.on('connection', (socket)=>{
 	console.log('User connected');
@@ -17,8 +20,12 @@ io.on('connection', (socket)=>{
 		console.log('User disconnected');		
 	});
 	socket.on('message_sent', (message)=>{
-		messages.push(message);
+		//messages.push(message);
 		io.emit('message_received', message);
+	});
+	socket.on('message_approved', (message)=>{
+		messages.push(message);
+		io.emit('message_approved', message);
 	});
 });
 
