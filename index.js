c79156b135dac6487eb203b605bf3f748105416e
RajatP314 <10013174@sbstudents.org>
2019-02-20 21:48:06 -0500

    added super rudimentary password lock for admin page

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let sha256 = require('js-sha256').sha256;
+let auth = require('express-basic-auth');
 
 let messages = [];
 
+function passwordChecker(username, password){
+	return username === "admin" && sha256(password) === "2492008b9a71e79b01f2281a8ab726f723204f07aceb541eddbb9ed60d813a06";
+}
+
 app.use(express.static('res'));
+app.use('/admin', auth({
+	authorizer: passwordChecker
+}));
 
 app.all('/', (req, res)=>{
 	res.sendFile(__dirname + "/index.html");
 });
-app.all('/admin', (req, res)=>{
+app.post('/admin', (req, res)=>{
 	res.sendFile(__dirname + "/admin.html");
+});
+app.all('/login', (req, res)=>{
+	res.sendFile(__dirname + "/login.html");
 });
 
 io.on('connection', (socket)=>{
